4d8e852c33e232dc71470f09eb0b71d9a8d95ba4
hetdv <hetdvx@gmail.com>
2025-01-19 00:29:33 +0100

    SUIIIIIIIIIII

--- src/components/HashiGame.tsx
+++ src/components/HashiGame.tsx
@@ -1,7 +1,5 @@
 'use client';
 import React, { useState, useEffect, useCallback } from 'react';
-import Image from 'next/image';
-
 /** --------------------------
  * 1) TYPE DEFINITIONS
  * --------------------------*/
